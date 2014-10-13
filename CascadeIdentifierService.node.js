fcfacb897dc86209a94321e643bf76332198b140
Don Undeen <don.undeen@metmuseum.org>
2014-10-13 18:52:35 +0000

    working on visualizer for identfier

--- CascadeIdentifierService.node.js
+++ CascadeIdentifierService.node.js
@@ -6,6 +6,9 @@ console.log("starting");
 var url = require('url');
 var fs = require("fs");
 var request = require("request");
+var urlparser = require("url");
+var pathparser = require("path");
+
 
 var Percolator = require('percolator').Percolator;
 
@@ -31,18 +34,30 @@ var download = function(uri, filename, callback){
 
 
 
-server.route('/', {  
+server.route('*', {  
 	GET : function(req, res){
+    var url_parts = url.parse(req.url, true);
+    var parsed = urlparser.parse(req.url, true)
+
+    var query = url_parts.query;
+    var imageUrl = query.imageurl;
+    var results = {foo : imageUrl};
+    var tempname = "tmp"+Date.now()+ Math.random();
+
+		if(req.url.match(/index\.html/i)){
+			if(!query.action){
+			  // this is doing it client-side
+			  sendFile(parsed.pathname, query, res);
+			  return;
+			}
+		}
+ 
+
 
 		console.log("getting identify");
-		var url_parts = url.parse(req.url, true);
-		var query = url_parts.query;
-		var imageUrl = query.imageurl;
-		var results = {foo : imageUrl};
-		var tempname = "tmp"+Date.now()+ Math.random();
 
 		download(imageUrl, tempname, function(){
-			Indentifier.match_all_cascades(image, function(results){
+			Indentifier.match_all_cascades(tempname, function(results){
 				fs.unlink(tempname);
     			res.object(results).send();
 			});
@@ -59,6 +74,64 @@ server.route('/', {
 server.listen(function(err){
   console.log('server is listening on port ', port);
 });
+
+
+
+var dataCache = {};
+function sendFile(path, query, res){
+
+  if(path == "/"){
+    path = "/index.html";
+  }
+
+  var extname = pathparser.extname(path);
+  var contentType = 'text/html';
+  if(path.match(/secrets\.js/)){
+        res.writeHead(404, {'Content-Type': contentType});
+        //indexhtml = data;
+        res.end("I'm afraid I can't do that.");
+        return;
+  }
+
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.jpg':
+      contentType = 'image/jpeg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.ico':
+      contentType = 'image/vnd.microsoft.icon';
+      break;
+  }
+
+  if(!dataCache[path]){
+    fs.readFile("."+path, function(err, data){
+      if(err){
+        console.log("file read error");
+        console.log(err);
+        res.writeHead(404, {'Content-Type': contentType});
+        //indexhtml = data;
+        res.end(data);
+      }else{
+        res.writeHead(200, {'Content-Type': contentType});
+        //dataCache[path] = data;
+        res.end(data);
+      }
+    });
+  }else{
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(dataCache[path]);
+  }
+}
+
+
 
 
 console.log("done");
